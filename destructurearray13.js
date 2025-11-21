2e49979b3eedef0236bfd44d83f0de9d747eb1a8
kurmiprashant67 <kurmiprashant67@gmail.com>
2025-11-21 18:07:23 +0530

    js

---
+++ destructurearray13.js
@@ -0,0 +1,10 @@
+let [a,b,c,d,e]=[1,2,4,5,6,]
+
+console.log(a);
+console.log(b);
+
+
+console.log(c);
+console.log(d);
+console.log(e);
+
--- formvalidation12.js
+++ formvalidation12.js
@@ -1,11 +1,16 @@
+
+                        
+
 function validate(){
 
     let name = document.querySelector('#name').value 
     let age = document.querySelector('#age').value 
-    
-    let content = document.querySelector('#contect').value
-    let gmail = document.querySelector('#gmail')
+    let Contact = document.querySelector('#Contact').value
+    let gmail = document.querySelector('#gmail').value
 
+
+    let pass = document.querySelector('#password').value
+    let cpass = document.querySelector('#cpassword').value
 
 
 if(name==""){
@@ -20,17 +25,44 @@ else if(isNaN(age)){
     return false
 }
 
-else if(isNaN(contect)){
-    alert("Please fill age in numbar ")
-    document.querySelector('#contect').focus()
+else if(isNaN(Contact)){
+    alert("Please fill Contact in numbar ")
+    document.querySelector('#Contact').focus()
     return false
 }
+else if(Contact>10 || Contact<10){
+    alert("invalid numbar , please check your numbar")
+    return false
 
+}
 else if( !(gmail,includes('@gmail.com'))){
-    alert("Please fill age in numbar ")
+    alert("Please fill gmail in numbar ")
     document.querySelector('#gmail').focus()
     return false
 }
 
 
+
+else if( !(pass.match(/[~!@#$%^&*]/)) ){
+    alert("Please enter one special char")
+    document.querySelector('#password').focus()
+    return false
 }
+
+else if(pass!=cpass){
+    alert("reenter password")
+     document.querySelector('#password').focus()
+     return false
+}
+
+
+}
+
+
+
+
+
+
+
+
+
